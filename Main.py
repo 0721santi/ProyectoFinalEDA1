6afa39f2becd356d0ae97f6fa3bf8354148f84ed
Santiago Idárraga Ceballos <san200526@gmail.com>
2023-11-19 16:06:06 -0500

    Func - Calculo de Frecuencia

--- Main.py
+++ Main.py
@@ -9,11 +9,18 @@ def generar_usuarios(total):
     dato = datos()
     return [Persona(random.randint(1000, 2000), random.choice(dato.nombres)+' '+random.choice(dato.apellidos), random.randint(18, 70)) for i in range(total)]
 
+def calcula_frecuencia(msgs, lastTime, time, frecuencia):
+    sum = frecuencia*msgs
+    tiempo = time-lastTime
+    sum += tiempo
+    frecuencia = sum/(msgs+1)
+    return frecuencia
+
 def generar_mensajes(us1, us2):
     users = [us1, us2]
     mensajes = []
     cantidad = random.randint(1, 100)
-    lastTime = 0
+    frecuencia = 0
     time = 0
     for i in range(cantidad):
         envia = random.choice(users)
@@ -21,12 +28,14 @@ def generar_mensajes(us1, us2):
             recibe = us2
         else:
             recibe = us1            
+        lastTime = time
         time = random.randint(time, time+60)
         mensajes.append(Mensaje(envia, recibe, time))
+        frecuencia = calcula_frecuencia(i, lastTime, time, frecuencia)
         if time+60 >= 1440:
             break
         historial.nuevoMsg(mensajes[i])
-    return mensajes
+    return mensajes, frecuencia
 
 def generar_amistades(total):
     global amistades, usuarios_amigos
@@ -42,13 +51,14 @@ def generar_amistades(total):
                 amistades.insert([(usuario, amigo), mensajes, random.randint(1, 12)])
         usuarios_amigos.insert([usuario, usuario.amigos])
 
-amistades = cola()
-usuarios_amigos = cola()
-historial = Historial()
-generar_amistades(20)
+if __name__ == "__main__":
+    amistades = cola()
+    usuarios_amigos = cola()
+    historial = Historial()
+    generar_amistades(20)
 
-for i in range(usuarios_amigos.size):
-    u = usuarios_amigos.eliminar()
-    print(u[0].nombre)
-    print(len(u[1]))
-    print()
+    for i in range(usuarios_amigos.size):
+        u = usuarios_amigos.eliminar()
+        print(u[0].nombre)
+        print(len(u[1]))
+        print()
