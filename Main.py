e2575a1c551b4646b3219378f7c2e05840490990
JohanAlv24 <127231234+JohanAlv24@users.noreply.github.com>
2023-11-19 18:10:24 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,18 @@
 from Persona import Persona
 from Mensaje import Mensaje
 from Datos import datos
-from Cola import cola
 from HistorialMensajes import HistorialMensajes as Historial
-import copy
+from Cola import cola
 import random
+
+
 def generar_usuarios(total):
     dato = datos()
     return [Persona(random.randint(1000, 2000), random.choice(dato.nombres)+' '+random.choice(dato.apellidos), random.randint(18, 70)) for i in range(total)]
-
-def calcula_frecuencia(msgs, lastTime, time, frecuencia):
-    sum = frecuencia*msgs
-    tiempo = time-lastTime
-    sum += tiempo
-    frecuencia = sum/(msgs+1)
-    return frecuencia
-
 def generar_mensajes(us1, us2):
     users = [us1, us2]
     mensajes = []
+    lista = []
     cantidad = random.randint(1, 100)
     frecuencia = 0
     time = 0
@@ -31,12 +25,12 @@ def generar_mensajes(us1, us2):
         lastTime = time
         time = random.randint(time, time+60)
         mensajes.append(Mensaje(envia, recibe, time))
-        frecuencia = calcula_frecuencia(i, lastTime, time, frecuencia)
+        frecuencia += time-lastTime 
+        historial.nuevoMsg(mensajes[i])
         if time+60 >= 1440:
             break
-        historial.nuevoMsg(mensajes[i])
+    frecuencia/=cantidad
     return mensajes, frecuencia
-
 def generar_amistades(total):
     global amistades, usuarios_amigos
     usuarios = generar_usuarios(total)
@@ -47,18 +41,41 @@ def generar_amistades(total):
             if amigo not in usuario.amigos and amigo!=usuario:
                 usuario.amigos.append(amigo)
                 amigo.amigos.append(usuario)
-                mensajes = generar_mensajes(usuario, amigo)
-                amistades.insert([(usuario, amigo), mensajes, random.randint(1, 12)])
-        usuarios_amigos.insert([usuario, usuario.amigos])
+                mensajes, frecuencia = generar_mensajes(usuario, amigo)
 
-if __name__ == "__main__":
-    amistades = cola()
-    usuarios_amigos = cola()
+                amistades.insert([(usuario, amigo), mensajes, int(frecuencia)])
+    historial.agrega_json()
+
+    for j in usuarios:
+        usuarios_amigos.insert([j, j.amigos])
+if __name__ == '__main__':
     historial = Historial()
-    generar_amistades(20)
+    amistades = cola()
+    #amistades es la cantidad de mensajes entre dos amigos y su frecuencia 
+    usuarios_amigos = cola()
+    #usuarios_amigos es la cantidad de amigos de cada usuario
+    print("Digite la cantidad de usuarios para la simulación:")
+    total = int(input())
+    generar_amistades(total)
 
-    for i in range(usuarios_amigos.size):
+    informe = []
+    informe.append('LOS USUARIOS CON MÁS AMIGOS SON: \n\n')
+
+    for i in range(3):
         u = usuarios_amigos.eliminar()
-        print(u[0].nombre)
-        print(len(u[1]))
-        print()
+        informe.append(f'{u[0].nombre} con {len(u[1])} amigos')
+        informe.append(f'\nSus amigos son:\n')
+
+        for j in u[0].amigos:
+            informe.append(f'{j.nombre} \n')
+        informe.append('\n\n')
+    informe.append('LOS MEJORES AMIGOS SON: \n\n')
+    for i in range(10):
+        u = amistades.eliminar()
+        informe.append(f'{u[0][0].nombre} y {u[0][1].nombre} con una cantidad de {len(u[1])} mensajes y {u[2]} mensajes por minuto \n\n')
+
+    with open("Informe.txt", 'w', encoding='utf_8') as analisis:
+        for a in informe:
+            analisis.write(a)
+
+
